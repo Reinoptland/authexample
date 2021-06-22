4643f79d18b1b9f3bcf09e325c312782a5ac4d26
Reinoptland <reinoptland@gmail.com>
2021-06-22 16:37:33 +0200

    Add loading indicator

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function SignUp() {
   // - [x] Installeer axios
@@ -11,35 +12,42 @@ function SignUp() {
   // - [x] In de try: maak een POST request naar het eindpoint: http://localhost:3000/register
   // - [x] Een POST request krijgt altijd de url en het data object mee (in dit geval minimaal email en wachtwoord)
   // - [x] Laat de gebruiker weten dat het registeren is gelukt
-  // - [ ] Stuur de gebruiker na twee seconden door naar het inlog-formulier
-  // - [ ] Puntjes op de i: error en laad-tijden implemententeren
+  // - [x] Stuur de gebruiker na twee seconden door naar het inlog-formulier
+  // - [x] Puntjes op de i: error en laad-tijden implemententeren
+  const history = useHistory();
   const { handleSubmit, register } = useForm();
-  const [successMessage, setSuccesMessage] = useState("");
+  const [success, setSucces] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(data) {
     console.log("DATA VAN DE GEBRUIKER??", data);
     try {
+      setLoading(true);
       const response = await axios.post("http://localhost:3000/register", {
         email: data.email,
         username: data.username,
         password: data.password,
       });
       console.log(response);
-      setSuccesMessage("Registeren is gelukt!");
+      setSucces(true);
+      setTimeout(() => history.push("/signin"), 2000);
     } catch (error) {
       console.log("OH NO", error);
     }
+
+    setLoading(false);
   }
 
   return (
     <>
       <h1>Registreren</h1>
-      <p>{successMessage}</p>
+      <p>{success && "Registeren is gelukt!"}</p>
+      <p>{loading && "Moment geduld aub"}</p>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum
         debitis dolor dolore fuga id molestias qui quo unde?
       </p>
-      {!successMessage && (
+      {!success && (
         <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="email-field">
             Email:
@@ -70,7 +78,7 @@ function SignUp() {
               {...register("password")}
             />
           </label>
-          <button type="submit" className="form-button">
+          <button disable={loading} type="submit" className="form-button">
             Maak account aan
           </button>
         </form>
