34d31a6ba3ee56bc2a34554081d2c791bffc0015
Reinoptland <reinoptland@gmail.com>
2021-06-22 16:24:01 +0200

    Set succes message to show user what happened

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
@@ -10,10 +10,11 @@ function SignUp() {
   // - [x] Maak een try / catch blok
   // - [x] In de try: maak een POST request naar het eindpoint: http://localhost:3000/register
   // - [x] Een POST request krijgt altijd de url en het data object mee (in dit geval minimaal email en wachtwoord)
-  // - [ ] Laat de gebruiker weten dat het registeren is gelukt
+  // - [x] Laat de gebruiker weten dat het registeren is gelukt
   // - [ ] Stuur de gebruiker na twee seconden door naar het inlog-formulier
   // - [ ] Puntjes op de i: error en laad-tijden implemententeren
   const { handleSubmit, register } = useForm();
+  const [successMessage, setSuccesMessage] = useState("");
 
   async function onSubmit(data) {
     console.log("DATA VAN DE GEBRUIKER??", data);
@@ -24,6 +25,7 @@ function SignUp() {
         password: data.password,
       });
       console.log(response);
+      setSuccesMessage("Registeren is gelukt!");
     } catch (error) {
       console.log("OH NO", error);
     }
@@ -32,44 +34,47 @@ function SignUp() {
   return (
     <>
       <h1>Registreren</h1>
+      <p>{successMessage}</p>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum
         debitis dolor dolore fuga id molestias qui quo unde?
       </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email-field">
-          Email:
-          <input
-            type="email"
-            id="email-field"
-            name="email"
-            {...register("email")}
-          />
-        </label>
+      {!successMessage && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="email-field">
+            Email:
+            <input
+              type="email"
+              id="email-field"
+              name="email"
+              {...register("email")}
+            />
+          </label>
 
-        <label htmlFor="username-field">
-          Gebruikersnaam:
-          <input
-            type="text"
-            id="username-field"
-            name="username"
-            {...register("username")}
-          />
-        </label>
+          <label htmlFor="username-field">
+            Gebruikersnaam:
+            <input
+              type="text"
+              id="username-field"
+              name="username"
+              {...register("username")}
+            />
+          </label>
 
-        <label htmlFor="password-field">
-          Wachtwoord:
-          <input
-            type="password"
-            id="password-field"
-            name="password"
-            {...register("password")}
-          />
-        </label>
-        <button type="submit" className="form-button">
-          Maak account aan
-        </button>
-      </form>
+          <label htmlFor="password-field">
+            Wachtwoord:
+            <input
+              type="password"
+              id="password-field"
+              name="password"
+              {...register("password")}
+            />
+          </label>
+          <button type="submit" className="form-button">
+            Maak account aan
+          </button>
+        </form>
+      )}
       <p>
         Heb je al een account? Je kunt je <Link to="/signin">hier</Link>{" "}
         inloggen.
